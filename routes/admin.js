865b06b26bbff538527ca0c11be5885fcf108b64
abhijnanacharya <abhijnanacharya11@gmail.com>
2021-12-07 21:35:16 +0530

    Static Webpage with CSS added

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,18 @@
+const path=require('path');
 const express=require('express');
 const router= express.Router();
+const rootDir=require('../util/path');
 
 router.get("/add-product",(req,res,next)=>{
-    res.send(`<form action="/product" method ="POST"><input type="text" name="title">
-    <select name="size" id="size" >
-        <option value="2">2</option>
-        <option value="4">4</option>
-        <option value="6">6</option>
-        <option value="8">8</option>
-        <option value="10">10</option>
-    </select>
-        <button type="submit">Add Product</button></form>`);
+   // res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+   res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
-router.post('/product',(req,res, next)=>{
+router.post("/add-product",(req,res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
 
-router.use((req,res,next)=>{
-    res.status(404).send(`INTERNETZ BROKEN: YOU SUCK!`)
-});
+
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,16 @@
 const express =require('express');
+const path=require('path');
 const router=express.Router();
+const rootDir=require('../util/path');
 
 router.get("/",(req,res,next)=>{
-    res.send(`<h1>Welcome Home!!</h1>`);
-    
+    res.sendFile(path.join(rootDir,'views','shop.html'));
+
 });
+
+// router.use((req,res,next)=>{
+//     res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+//     console.log(404);
+// });
 
 module.exports = router;
