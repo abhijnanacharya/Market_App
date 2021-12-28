164635d030122d633e06568b5af7d6c0d9cb0c24
abhijnanacharya <abhijnanacharya11@gmail.com>
2021-12-29 01:53:29 +0530

    Responsive Controllers added to update and edit Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,15 +57,29 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price=updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+
+      return product.save();
+    })
+    .then(res=> {
+      console.log("Product UPDATED Successfully!!");
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updatedImageUrl,
+  //   updatedDesc,
+  //   updatedPrice
+  // );
+  //updatedProduct.save();
+  // res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
